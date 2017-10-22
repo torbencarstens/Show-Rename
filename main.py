dbb0d0193ba69787ab01d6460602e3c8f73be501
chabare <chabare95@gmail.com>
2017-10-22 19:16:46 +0200

    Remove test code

--- main.py
+++ main.py
@@ -16,16 +16,4 @@ from docopt import docopt
 if __name__ == "__main__":
     args = docopt(__doc__)
     directory, show_name = args["--directory"], args["--name"]
-
-    imdb = Imdb()
-    results = list(filter(lambda x: x['title'] == show_name, imdb.search_for_title(show_name)))
-
-    titles = []
-    for idx, result in enumerate(results):
-        titles.append(imdb.get_title_by_id(result['imdb_id']))
-
-    for title in titles:
-        print(
-            "Title: {} - Year: {} - Type: {} - Genres: {} - Plot: {}".format(title.title, title.year, title.type,
-                                                                             title.genres, title.plot_outline))
-        # main(directory, show_name, 2009)
+    main(directory, show_name, 2009)
