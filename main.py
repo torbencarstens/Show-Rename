88a6e8715dec1bacd906da45a5701a0890844f42
chabare <chabare95@gmail.com>
2017-10-28 22:01:30 +0200

    Remove custom year

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ Options:
 
 from imdbpie import Imdb
 from docopt import docopt
+from rename_show.rename import main
 
 # from rename_show.rename import main
 
 if __name__ == "__main__":
     args = docopt(__doc__)
     directory, show_name = args["--directory"], args["--name"]
-    main(directory, show_name, 2009)
+    main(directory, show_name)
