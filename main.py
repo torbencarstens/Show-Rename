682684f365ea00b9f94720c181d52b0ba9eaf6be
chabare <chabare95@gmail.com>
2017-11-13 15:35:15 +0100

    Add rename_to for search != wanted filename

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Options:
     -d --directory=<directory>
     -f --file-ext=<file-ext>
     --strict
+    --rename-to=<rename-to>
 """
 
 from docopt import docopt
@@ -18,10 +19,11 @@ from rename_show.rename import main
 if __name__ == "__main__":
     args = docopt(__doc__)
     directory, show_name, file_ext = args["--directory"], args["--name"], args["--file-ext"]
+    rename_to = args['--rename-to']
     strict = 'nostrict' in args.keys()
     if not file_ext:
         file_ext = ".mkv"
     elif not file_ext.startswith("."):
         file_ext = ".{}".format(file_ext)
 
-    main(directory, show_name, file_ext=file_ext, strict=strict)
+    main(directory, show_name, file_ext=file_ext, strict=strict, rename_to=rename_to)
