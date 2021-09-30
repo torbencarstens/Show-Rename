f0e57bb7dab355c3ebfc43de6e85136f8e12e0b6
Torben Carstens <git@carstens.tech>
2021-09-30 18:04:24 +0200

    Change default argument handling

--- main.py
+++ main.py
@@ -24,23 +24,19 @@ from rename_show.rename import main
 # noinspection PyShadowingNames
 def validate_options():
     args = docopt(__doc__)
-    directory, show_name, file_ext = args["--directory"], args["--name"], args["--file-ext"]
+    directory = args["--directory"] or "."
+    show_name, file_ext = args["--name"], args["--file-ext"]
     rename_to = args['--rename-to']
     season = args['--season']
     skip_first = args['--skip-first-episode'] or False
     confirm_renaming = args['--confirm'] or False
     strict = 'strict' in args.keys()
-    custom_format = args['--custom-format']
-    if not file_ext:
-        file_ext = "mkv"
-    elif file_ext.startswith("."):
+    custom_format = args['--custom-format'] or "mkv"
+    if file_ext.startswith("."):
         file_ext = file_ext[1:]
 
-    if not directory:
-        directory = "."
-    else:
-        if not os.path.exists(directory):
-            raise ValueError("directory does not exist")
+    if not os.path.exists(directory):
+        raise ValueError("directory does not exist")
 
     if not show_name:
         raise ValueError("No show_name has been provided")
